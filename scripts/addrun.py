602178f8140df53878c327f40d45d3cd4dbef9ec
Zabamund <fracgeol@gmail.com>
2021-01-22 12:37:09 +0100

    Add automate date insertion

--- scripts/addrun.py
+++ scripts/addrun.py
@@ -1,13 +1,18 @@
 import sys
 import re
+from datetime import date
 
 PATH = '/home/geodev/Data/code/misc/data/runlog'
+TODAY = date.today()
 
 def test_newrun(newrun):
-    pattern = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{1,3}\.\d{2}'
-    fout = 'New run must follow the exact pattern: `"YYYY-MM-DD HH:MM:SS,KK.MM"`'
-    assert re.findall(pattern, newrun), fout
-
+    pattern_full = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{1,3}\.\d{2}'
+    pattern_today = r'\d{2}:\d{2}:\d{2},\d{1,3}\.\d{2}'
+    if re.findall(pattern_full, newrun):
+        return re.findall(pattern_full, newrun)[0]
+    else:
+        return f'{TODAY} {re.findall(pattern_today, newrun)[0]}'
+    
 def read_runlog(PATH, newrun):
     """
     Add a run to PATH file
@@ -19,14 +24,18 @@ def read_runlog(PATH, newrun):
         newrun += '\n'
         f.write(newrun)
 
-    print('Run added to runlog.')
+    print(f'Run: "{newrun.strip()}" added to runlog.')
     return
 
 
 if __name__ == '__main__':
     try:
-        newrun = sys.argv[1]
-        test_newrun(newrun)
+        newrun = test_newrun(sys.argv[1])
         read_runlog(PATH, newrun)
     except IndexError:
-        print('New run must follow the exact pattern: `"YYYY-MM-DD HH:MM:SS,KK.MM"`')
+        print("""New run must be provided as either:
+`"YYYY-MM-DD HH:MM:SS,KK.MM"`
+or
+`"HH:MM:SS,KK.MM"`
+in which case TODAY\'s date will be inserted automatically.""")
+        
