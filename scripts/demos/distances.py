92a633c9aecea62127021280186137399f14a75c
Zabamund <fracgeol@gmail.com>
2021-01-26 10:33:38 +0100

    Renamde distances file as Minkowski added

--- scripts/demos/distances.py
+++ scripts/demos/distances.py
@@ -13,3 +13,11 @@ def euclidian_dist(ax, ay, bx, by):
     return np.sqrt((bx - ax)**2 + (by - ay)**2)
 
 
+def minkowski_dist(xs, ys, p=1):
+    xs, ys = np.array(xs), np.array(ys)
+    if np.isinf(p):
+        return np.max(np.abs(xs - ys))
+    elif np.isneginf(p):
+        return np.min(np.abs(xs - ys))
+    else:
+        return np.sum(np.abs(xs - ys)**p)**(1/p)
