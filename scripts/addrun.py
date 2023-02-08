a58fdacc836a4219792bbc95bad389075933572d
Zabamund <fracgeol@gmail.com>
2023-02-08 09:52:30 +0100

    refactor addrun for new shoes

--- scripts/addrun.py
+++ scripts/addrun.py
@@ -14,28 +14,31 @@ def test_newrun(newrun):
     Args:
         newrun (str): a string with or without date
                       also indicating gear type
-        gear type is one of {'o', 'k', 'p'}
-        for on, kinvara or peregrine
+        gear type is one of {'kw', 'kb, 'p'}
+        for linvara_white, kinvara_blue or peregrine
     """
-    pattern_full = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{1,3}\.\d{2},[o,k,p]?'
-    pattern_today =                  r'\d{2}:\d{2}:\d{2},\d{1,3}\.\d{2},[o,k,p]?'
-    if re.findall(pattern_full, newrun):
-        return re.findall(pattern_full, newrun)[0]
+    pattern_full = r'\d{4}-\d{2}-\d{2},\d{2}:\d{2}:\d{2},\d{1,3}\.\d{1,2},(kw|kb|p)?'
+    pattern_today =                  r'\d{2}:\d{2}:\d{2},\d{1,3}\.\d{1,2},(kw|kb|p)?'
+    if re.fullmatch(pattern_full, newrun):
+        return re.fullmatch(pattern_full, newrun).group()
     else:
-        return f'{TODAY} {re.findall(pattern_today, newrun)[0]}'
+        return f'{TODAY},{re.fullmatch(pattern_today, newrun).group()}'
 
 def read_runlog(PATH, newrun):
     """
     Add a run to PATH file
     Args:
         PATH: absolute or relative path to file, [str]
-        newrun: run details in form `'YYYY-MM-DD HH:MM:SS,KK.MM'`
+        newrun: run details in form `'YYYY-MM-DD,HH:MM:SS,KK.MM'`
     """
+    timedate = ' '.join(newrun.split(',')[:2])
+    distance = newrun.split(',')[2]
+    newrun = ','.join([timedate, distance])
     with open(PATH, 'a') as f:
         newrun += '\n'
         f.write(newrun)
 
-    print(f'Run: "{newrun.strip()}" added to runlog.')
+    print(f'Run: "{newrun}" added to runlog.')
     return
 
 
@@ -43,22 +46,22 @@ def update_gear_mileage(PATH_GEAR, newrun):
     """Upate mileage for a given piece of gear.
     Args:
         PATH: absolute or relative path to file, [str]
-        newrun: run details in form `'YYYY-MM-DD HH:MM:SS,KK.MM,[o,k,p,t,b]'`
+        newrun: run details in form `'YYYY-MM-DD,HH:MM:SS,KK.MM,[kw,kb,p,t,b]'`
     Notes:
-        gear type is one of {'o', 'k', 'p', 't', 'b'}
-        o and k are the main road running shoes when either there is an overlap
+        gear type is one of {'kw', 'kb', 'p', 't', 'b'}
+        kw and kb are the main road running shoes when either there is an overlap
         between an old and new pair, or if there are two pairs to prepare for a
         long race.
         for road, trail, trek or bike
     """
-    colnames = {'o': 'oncloud',
-                'k': 'kinvara',
+    colnames = {'kb': 'kinvara_blue',
+                'kw': 'kinvara_white',
                 'p': 'peregrine',
                 't': 'trek_shoes',
                 'b': 'bike'
                 }
     gear = newrun.split(',')[-1]
-    kms = float(newrun.split(',')[1])
+    kms = float(newrun.split(',')[-2])
     df = pd.read_csv(PATH_GEAR)
     df.loc[0, colnames.get(gear)] += kms
     df.to_csv(PATH_GEAR, index=False)
@@ -70,12 +73,12 @@ def update_gear_mileage(PATH_GEAR, newrun):
 if __name__ == '__main__':
     try:
         newrun = test_newrun(sys.argv[1])
-        if newrun.split(',')[-1] in ['o', 'k', 'p']:
+        if newrun.split(',')[-1] in ['kw', 'kb', 'p']:
             read_runlog(PATH, newrun)
         update_gear_mileage(PATH_GEAR, newrun)
     except IndexError:
         print("""New run must be provided as either:
-`"YYYY-MM-DD HH:MM:SS,KK.MM,[o,k,p,k,b]"`
+`"YYYY-MM-DD HH:MM:SS,KK.MM,[kw,kb,p,t,b]"`
 or
-`"HH:MM:SS,KK.MM,[o,k,p,k,b]"`
+`"HH:MM:SS,KK.MM,[kw,kb,p,t,b]"`
 in which case TODAY\'s date will be inserted automatically.""")
